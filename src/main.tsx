22307eb9e2aeec5b3c9a115eae628fd67259b5f9
Yashwanth Varma <yashwanth.sse@gmail.com>
2026-03-09 22:39:52 +0530

    Changes in download

--- src/main.tsx
+++ src/main.tsx
@@ -3,3 +3,10 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("/sw.js")
+        .then(() => console.log("Service Worker registered"));
+    });
+  }
